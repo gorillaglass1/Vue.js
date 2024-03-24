fffec70cb8cae51f206d89a6f2ffd423f9c11cc2
gorillaglass1 <155641361+gorillaglass1@users.noreply.github.com>
2024-03-24 22:08:21 +0900

    Add files via upload

---
+++ vuedongsan/App.vue
@@ -0,0 +1,124 @@
+<template>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용임</p>
+      <botton @click="모달창열렸니 = false">닫기</botton>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{작명}}</a> <!--작명 부분이 array 안 element가 들어가고 array element 갯수만큼 반복됨, key의 역할 반복문 돌린 요소를 구분-->
+  </div>
+
+  <div>
+    <img class="roomImage" :src="원룸들[0].image">
+    <h4 @click="모달창열렸니 = true">{{  원룸들[0].title }}</h4>
+    <p>{{ 원룸들[0].price }}원</p>
+    <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{ 신고수[0] }}</span>
+  </div>
+  <div>
+    <img class="roomImage" src="./assets/room1.jpg">
+    <h4>{{ products[1] }}</h4>
+    <p>70만원</p>
+    <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
+  </div>
+    <div>
+    <img class="roomImage" src="./assets/room2.jpg">
+    <h4>{{ products[2] }}</h4>
+    <p>80만원</p>
+    <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
+  </div>
+
+  <!-- <div>
+    <h4>{{ products[0] }}</h4>
+    <p>60만원</p>
+    <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{ 신고수[0] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>70만원</p>
+    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
+  </div>
+    <div>
+    <h4>{{ products[2] }}</h4>
+    <p>80만원</p>
+    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
+  </div> -->
+</template>
+
+<script>
+  
+
+import data from './assets/oneroom.js';
+
+//import {apple, apple2} from './assets/oneroom.js'; //apple은 마음대로 작명가능;
+//apple;
+//apple2;
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      //데이터 보관함
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  methods : {
+    increase(num) {
+      this.신고수[num]++ ;
+    },
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-block: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.roomImage {
+  width: 100;
+  margin-top: 40px;
+}
+</style>
---
+++ vuedongsanself/App.vue
@@ -0,0 +1,125 @@
+<template>
+  <img class="vue" alt="Vue logo" src="./assets/logo.png">
+
+  <div class="black-bg" v-if="alert == true">
+    <div class="white-bg">
+      <h4>안내 사항</h4>
+      <p>부동산 <span style="color: red;">사기에</span> 주의하십시오.</p>
+      <button @click="alert = false">닫기</button>
+    </div>
+  </div>
+
+  <div class="black-bg" v-if="Inquirealert == true">
+    <div class="white-bg">
+      <h4>문의</h4>
+      <p>해당 부동산의 판매자를 
+        <span style="color: red;">찾을 수 없습니다.</span></p>
+      <button @click="Inquirealert = false">닫기</button>
+    </div>
+  </div>
+
+  <div class="black-bg" v-if="reportsub == true">
+    <div class="white-bg">
+      <h4>신고접수</h4>
+      <p>신고당한 내용을 적어주세요.</p>
+      <p><input type="text" name="report"> <br>
+      <input type="submit" name="sent">&nbsp;</p>
+      <button @click="reportsub = false">취소</button>
+    </div>
+  </div>
+
+
+  <div class="menu">
+    <a class="menu">뷰동산</a><br><br>
+    <a v-for="(a, i) in menu" :key="i">
+    {{ a }}
+    </a>
+  </div>
+
+  <div v-for="(a, i) in source" :key="i">
+    <hr>
+    <img class="roomimage" :src = 'a.image'>
+    <h4>원룸: {{ a.title }}</h4>
+    <p>특징: {{ a.content }}</p>
+    <p>가격: {{ a.price }}원 </p>
+    <p>
+      <button @click="Inquirealert = true">문의하기</button>
+    </p>
+    <p>신고횟수: {{ report[i] }}회 <button @click="increase(i)">신고하기</button></p>
+    <p><button @click="reportsub = true">신고 접수하기</button></p>
+  </div>
+  
+</template>
+
+<script>
+
+import oneroomdata from './assets/oneroom.js';
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      source : oneroomdata,
+      alert : true,
+      Inquirealert : false,
+      report : [0, 0, 0, 0, 0, 0],
+      reportsub : false,
+      menu : ['Home', 'Shop', 'About'],
+    }
+  },
+  methods: {
+    increase(num) {
+      this.report[num]++ ;
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.vue {
+  width: 50px;
+  height: 50px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-block: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.roomImage {
+  width: 100;
+  margin-top: 40px;
+}
+</style>
